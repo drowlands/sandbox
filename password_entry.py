839d4d4e57d1dbfed3d4728ba517498fc8ff9af4
Daniel R <drowlands52@gmail.com>
2019-08-18 18:55:31 +1000

    Add password check program.

--- password_entry.py
+++ password_entry.py
@@ -1,3 +1,18 @@
 '''
 Daniel Rowlands Molpeceres
 '''
+
+min_length = int(10)
+password = ""
+done = False
+
+while not done:
+    password = input("Input a password: ")
+    if len(password) < min_length:
+        print("Password is too short. Please try again.")
+    else:
+        output = ""
+        for step in range(len(password)):
+            output += "*"
+        print(output)
+        done = True
